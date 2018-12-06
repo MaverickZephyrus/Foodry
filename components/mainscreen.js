9e926974b55e163bb7e94843e7ed24fa9e1f4018
MaverickZephyrus <lazytagam.1@gmail.com>
2018-12-05 18:36:07 -0800

    update

--- components/mainscreen.js
+++ components/mainscreen.js
@@ -279,7 +279,7 @@ class MainScreen extends React.Component {
             this.setState({ modalVisible: false }),
             this.props.navigation.navigate('Details', {'img' : this.state.item_data.img, 'food_name': this.state.item_data.food_name, 'cost': this.state.item_data.cost, 'notes': this.state.item_data.notes, 'restaurant': this.state.item_data.restaurant, 'date': this.state.item_data.date })
 
-            }}><Bold>Edit</Bold></Text>
+            }}><Bold>More...</Bold></Text>
 
                         </ScrollView>
                         </BlurView>
